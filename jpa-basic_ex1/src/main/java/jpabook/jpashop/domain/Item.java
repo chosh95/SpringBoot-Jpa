a0aac228fabfaf61419dfa054655cf9edeb08480
chosh95 <chosh-95@hanmail.net>
2020-06-13 12:32:56 +0900

    join

--- jpa-basic_ex1/src/main/java/jpabook/jpashop/domain/Item.java
+++ jpa-basic_ex1/src/main/java/jpabook/jpashop/domain/Item.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn
 public abstract class Item {
 
