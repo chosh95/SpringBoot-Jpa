f11b7e4eea94fe772b73dacfdde8573d5dd90324
chosh95 <chosh-95@hanmail.net>
2020-08-01 18:15:09 +0900

    jpa

--- springboot-basic/src/main/java/hello/hellospring/domain/Member.java
+++ springboot-basic/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue
     private Long id;
+
     private String name;
 
     public Long getId() {
